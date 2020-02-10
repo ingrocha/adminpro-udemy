f81e88a3567c1be84c63967ec005f3b23aa568a0
ingrocha <ing_rocha@hotmail.com>
2020-02-09 21:40:22 -0700

    Avance en casa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -26,7 +26,8 @@ import { ServiceModule } from './services/service.module';
     PagesModule,
     APP_ROUTES,
     ServiceModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
   ],
